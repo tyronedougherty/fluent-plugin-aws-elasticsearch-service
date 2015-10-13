f1fb06549b74257493345f393d1fdf1497bd6c3d
atomita <sleeping.cait.sith+gh@gmail.com>
2015-10-13 20:07:43 +0900

    極力書き換えないでやるversion

--- lib/fluent/plugin/out_aws-elasticsearch-service.rb
+++ lib/fluent/plugin/out_aws-elasticsearch-service.rb
@@ -19,56 +19,56 @@ module Fluent
       config_param :secret_access_key, :string, :default => ""
     end
 
-    config_param :logstash_format, :bool, :default => false
-    config_param :logstash_prefix, :string, :default => "logstash"
-    config_param :logstash_dateformat, :string, :default => "%Y.%m.%d"
-    config_param :utc_index, :bool, :default => true
-    config_param :type_name, :string, :default => "fluentd"
-    config_param :index_name, :string, :default => "fluentd"
-    config_param :id_key, :string, :default => nil
-    config_param :parent_key, :string, :default => nil
-    config_param :request_timeout, :time, :default => 5
-    config_param :reload_connections, :bool, :default => true
-    config_param :reload_on_failure, :bool, :default => false
-    config_param :time_key, :string, :default => nil
-    config_param :ssl_verify , :bool, :default => true
-    config_param :client_key, :string, :default => nil
-    config_param :client_cert, :string, :default => nil
-    config_param :client_key_pass, :string, :default => nil
-    config_param :ca_file, :string, :default => nil
-
-    include Fluent::SetTagKeyMixin
-    config_set_default :include_tag_key, false
-
-    def client
-      @_es ||=
-        begin
-          excon_options = { client_key: @client_key, client_cert: @client_cert, client_key_pass: @client_key_pass }
-          adapter_conf = lambda {|f| f.adapter :excon, excon_options }
-          #adapter_conf = lambda {|f| f.adapter :net_http}
-          transport = Faraday.new(
-            get_connection_options.merge(
-            options: {
-              reload_connections: @reload_connections,
-              reload_on_failure: @reload_on_failure,
-              retry_on_failure: 5,
-              transport_options: {
-                request: { timeout: @request_timeout },
-                ssl: { verify: @ssl_verify, ca_file: @ca_file }
-              }
-            }), &adapter_conf)
-          es = Elasticsearch::Client.new transport: transport
- 
-          begin
-            raise ConnectionFailure, "Can not reach Elasticsearch cluster (#{connection_options_description})!" unless es.ping
-          rescue *es.transport.host_unreachable_exceptions => e
-            raise ConnectionFailure, "Can not reach Elasticsearch cluster (#{connection_options_description})! #{e.message}"
-          end
- 
-          log.info "Connection opened to Elasticsearch cluster => #{connection_options_description}"
-          es
-        end
-    end
+#    config_param :logstash_format, :bool, :default => false
+#    config_param :logstash_prefix, :string, :default => "logstash"
+#    config_param :logstash_dateformat, :string, :default => "%Y.%m.%d"
+#    config_param :utc_index, :bool, :default => true
+#    config_param :type_name, :string, :default => "fluentd"
+#    config_param :index_name, :string, :default => "fluentd"
+#    config_param :id_key, :string, :default => nil
+#    config_param :parent_key, :string, :default => nil
+#    config_param :request_timeout, :time, :default => 5
+#    config_param :reload_connections, :bool, :default => true
+#    config_param :reload_on_failure, :bool, :default => false
+#    config_param :time_key, :string, :default => nil
+#    config_param :ssl_verify , :bool, :default => true
+#    config_param :client_key, :string, :default => nil
+#    config_param :client_cert, :string, :default => nil
+#    config_param :client_key_pass, :string, :default => nil
+#    config_param :ca_file, :string, :default => nil
+# 
+#    include Fluent::SetTagKeyMixin
+#    config_set_default :include_tag_key, false
+#
+#    def client
+#      @_es ||=
+#        begin
+#          excon_options = { client_key: @client_key, client_cert: @client_cert, client_key_pass: @client_key_pass }
+#          adapter_conf = lambda {|f| f.adapter :excon, excon_options }
+#          #adapter_conf = lambda {|f| f.adapter :net_http}
+#          transport = Faraday.new(
+#            get_connection_options.merge(
+#            options: {
+#              reload_connections: @reload_connections,
+#              reload_on_failure: @reload_on_failure,
+#              retry_on_failure: 5,
+#              transport_options: {
+#                request: { timeout: @request_timeout },
+#                ssl: { verify: @ssl_verify, ca_file: @ca_file }
+#              }
+#            }), &adapter_conf)
+#          es = Elasticsearch::Client.new transport: transport
+# 
+#          begin
+#            raise ElasticsearchOutput::ConnectionFailure, "Can not reach Elasticsearch cluster (#{connection_options_description})!" unless es.ping
+#          rescue *es.transport.host_unreachable_exceptions => e
+#            raise ElasticsearchOutput::ConnectionFailure, "Can not reach Elasticsearch cluster (#{connection_options_description})! #{e.message}"
+#          end
+# 
+#          log.info "Connection opened to Elasticsearch cluster => #{connection_options_description}"
+#          es
+#        end
+#    end
 
     def get_connecton_options
       raise "`endpoint` require." if @endpoint.empty?
@@ -82,8 +82,10 @@ module Fluent
               hash
             end
 
-            host[:credentials] = credentials(ep.access_key_id, ep.secret_access_key)
-            host[:region] = ep.region
+            host[:aws_elasticsearch_service] = {
+              :credentials => credentials(ep.access_key_id, ep.secret_access_key),
+              :region => ep.region
+            }
             
             host
           end
@@ -110,7 +112,7 @@ module Fluent
     end
 
 
-    class Faraday < Elasticsearch::Transport::Transport::HTTP::Faraday
+    class ElasticsearchOutput::Elasticsearch::Transport::Transport::HTTP::Faraday < ::Elasticsearch::Transport::Transport::HTTP::Faraday
       # Builds and returns a collection of connections.
       #
       # @return [Connections::Collection]
@@ -118,8 +120,8 @@ module Fluent
       def __build_connections
         Connections::Collection.new(
           :connections => hosts.map { |host|
-            host[:protocol]   = host[:scheme] || Elasticsearch::Transport::Transport::Base::DEFAULT_PROTOCOL
-            host[:port]     ||= Elasticsearch::Transport::Transport::Base::DEFAULT_PORT
+            host[:protocol]   = host[:scheme] || DEFAULT_PROTOCOL
+            host[:port]     ||= DEFAULT_PORT
             url               = __full_url(host)
 
             Connections::Connection.new(
@@ -139,10 +141,12 @@ module Fluent
 
       def faraday_conf(host, &block)
         lambda do |faraday|
-          faraday.request :aws_signers_v4,
-                          credentials: host.credentials,
-                          service_name: 'es',
-                          region: host.region
+          if host.aws_elasticsearch_service
+            faraday.request :aws_signers_v4,
+                            credentials: host.aws_elasticsearch_service.credentials,
+                            service_name: 'es',
+                            region: host.aws_elasticsearch_service.region
+          end
           block.call faraday
         end
       end
